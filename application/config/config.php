98bb476c8bcc1d287ea91b820c5e5b0d97f81260
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-07-23 14:16:42 +0700

    update login (view) : update html, css, bootstrap

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['base_url'] = 'http://localhost/digital_learning';
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '';
 $config['language']	= 'english';
--- application/views/index.php
+++ application/views/index.php
@@ -8,28 +8,37 @@
 
     <!-- css login -->
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/login.css">
-
+    
     <!-- bootstrap -->
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="container-background">
-        <div class="container-content-img">
-            <div class="content-img">
-                <img class="image" src="<?= base_url('assets/'); ?>image/login/icon_login.png" alt="">
-            </div>
-        </div>
+    <!-- Main Content -->
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
 
-        <div class="container-content-form">
-            <div class="content-form">
-                <input type="text" class="form-control" placeholder="Username">
-                <input type="password" class="form-control" placeholder="Password">
-                <div class="row justify-content-center">
-                    <div class="col-12 col-md-6 col-lg-6 rounded-pill">
-                        <button class="btn btn-secondary btn-block">Login</button>
-                    </div>
+        <div class="row box-area">
+            <!-- Left Content -->
+            <div class="col-md-6 d-flex justify-content-center align-items-center flex-column left-box mt-5">
+                <div class="featured-image mb-3">
+                    <img class="img-fluid" src="<?= base_url('assets/'); ?>image/login/icon_login.png" style="250px">
                 </div>
+            </div>
+
+            <!-- Right Content -->
+            <div class="col-md-6 d-flex justify-content-center align-items-center right-box mt-5">
+                    <div class="row align-items-center">
+                        <div class="input-group m-2">
+                            <input type="text" class="form-control form-control-lg bg-white fs-6" style="border-radius: 12px;" placeholder="Username">
+                        </div>
+                        <div class="input-group m-2">
+                            <input type="password" class="form-control form-control-lg bg-white fs-6" style="border-radius: 12px;" placeholder="Password">
+                        </div>
+                        <div class="d-flex input-group m-2 justify-content-center align-items-center rounded-sm mt-3">
+                            <!-- <button class="btn btn-lg btn-secondary w-50 fs-6" style="border-radius: 12px;">Login</button> -->
+                            <a href="<?= site_url('admin/beranda/page') ?>" class="btn btn-lg btn-secondary w-50 fs-6" style="border-radius: 12px; height:7.5vh">Login</a>
+                        </div>
+                    </div>
             </div>
         </div>
     </div>
