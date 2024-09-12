fecbbace2b5afaa74375891a253c5fd4f69cfc30
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-09-13 02:41:08 +0700

    update content

--- application/views/index.php
+++ application/views/index.php
@@ -16,10 +16,10 @@
 <body>
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
 
-        <div class="row box-area">
+        <div class="row box-area" style="border-radius:25px;border:1px solid black;">
             <!-- Left Content -->
             <div class="col-md-6 d-flex justify-content-center align-items-center flex-column left-box mt-5">
-                <div class="featured-image mb-3">
+                <div class="featured-image mb-3" style="margin-left: 1rem;">
                     <img class="img-fluid" src="<?= base_url('assets/'); ?>image/login/icon_login.png" width="200">
                 </div>
             </div>
@@ -27,18 +27,18 @@
             <!-- Right Content -->
             <div class="col-md-6 d-flex justify-content-center align-items-center right-box mt-5">
                 <form action="<?= site_url('login/auth'); ?>" method="post">
-                    <div class="row d-flex justify-content-center align-items-center w-full">
+                    <div class="row d-flex justify-content-center align-items-center w-full" style="margin-right: 1rem;">
                         <div class="d-flex justify-content-center align-items-center m-2">
                             <?php if ($this->session->flashdata('error')) : ?>
                                 <div class="alert alert-danger w-100"><?php echo $this->session->flashdata('error'); ?></div>
                             <?php endif; ?>
                         </div>
 
-                        <div class="input-group m-2 d-flex w-50">
-                            <input type="text" name="username" class="form-control form-control-lg bg-white fs-6 w-50" style="border-radius: 12px;" placeholder="Username" required="">
+                        <div class="input-group m-2 d-flex w-75">
+                            <input type="text" name="username" class="form-control form-control-lg bg-white fs-6 w-75" style="border-radius: 12px;" placeholder="Username" required="">
                         </div>
 
-                        <div class="input-group m-2 w-50">
+                        <div class="input-group m-2 w-75">
                             <input type="password" name="password" class="form-control form-control-lg bg-white fs-6" style="border-radius: 12px;height:5.5vh" placeholder="Password" required="">
                         </div>
 
