61f7322bf11edcac65e5e18731e360397ffa6192
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-09-18 14:17:21 +0700

    update content

--- application/views/siswa/beranda/index.php
+++ application/views/siswa/beranda/index.php
@@ -26,84 +26,153 @@
 
 <body>
     <div class="p-md-2">
+
+        <!-- carousel -->
         <div class="text-dark">
-            <div class="mt-2">
-                <div class="row">
-
-                    <div class="slide col-sm-6 col-md-4">
-                        <p class="mb-0">desktop 1</p>
-                        <div class="position-relative box-content bg-yellow box-custom">
-                            <p class="m-0">Selamat Datang di Digital</p>
-                            <p class="m-0">Learning Mata Pelajaran</p>
-                            <p class="m-0">Teknologi, Informasi, dan</p>
-                            <p class="m-0">Komunikasi (TIK)</p>
-                            <img src="<?= base_url('assets/image/siswa/world.png') ?>" class="position-absolute bottom-left-image">
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-md-4">
-                        <p class="mb-0">desktop 2</p>
-                        <div class="position-relative box-content bg-blue box-custom">
-                            <img src="<?= base_url('assets/image/siswa/tut_wuri_handayani.png') ?>" class="position-absolute top-left-image">
-                            <div class="mt-3">
-                                <p class="m-0">Belajar Jadi Lebih Mudah</p>
-                                <p class="m-0">dan Menyenangkan</p>
+            <div class="carousel slide overflow-hidden" data-bs-ride="carousel">
+                <div class="carousel-item active">
+                    <div class="mt-2">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-4">
+                                <p class="mb-0">desktop 1</p>
+                                <div class="position-relative box-content bg-yellow box-custom">
+                                    <div class="mt-3">
+                                        <p class="m-0">Selamat Datang di Digital Learning <br> Mata Pelajaran
+                                            Teknologi, Informasi,<br> dan Komunikasi (TIK)</p>
+                                        <img src="<?= base_url('assets/image/siswa/world.png') ?>" class="position-absolute bottom-left-image" style="margin-top: 2rem;" width="70">
+                                        <img src="<?= base_url('assets/image/siswa/stack_of_books.png') ?>" class="position-absolute bottom-right-image" style="margin-bottom: 2rem;" width="60">
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div class="col-sm-6 col-md-4">
-                        <p class="mb-0">desktop 3</p>
-                        <div class="d-flex box-content bg-pink box-custom">
-                            <p class="d-flex justify-content-center align-items-center">Waktunya Untuk Belajar
-                                “Dengan ilmu, kamu bisa meraih apa pun yang kamu impikan.</p>
+                            <div class="col-sm-6 col-md-4">
+                                <p class="mb-0">desktop 2</p>
+                                <div class="position-relative box-content bg-blue box-custom">
+                                    <div class="mt-4">
+                                        <p class="m-0">Belajar Jadi Lebih Mudah<br>dan Menyenangkan</p>
+                                        <img src="<?= base_url('assets/image/siswa/brain.png') ?>" class="position-absolute bottom-left-image" style="margin-top: 2rem;" width="110">
+                                        <img src="<?= base_url('assets/image/siswa/thinking.png') ?>" class="position-absolute bottom-right-image" style="margin-bottom: 3rem;" width="100">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-4">
+                                <p class="mb-0">desktop 3</p>
+                                <div class="position-relative box-content bg-pink box-custom">
+                                    <div class="mt-4">
+                                        <p class="m-0">Ayo, semangat belajar dan <br> raihlah
+                                            citamu setinggi langit!!!
+                                        </p>
+                                        <img src=" <?= base_url('assets/image/siswa/student.png') ?>" class="position-absolute bottom-left-image" style="margin-top: 3rem;" width="80">
+                                        <img src=" <?= base_url('assets/image/siswa/star.png') ?>" class="position-absolute bottom-right-image" style="margin-bottom: 2rem;" width="100">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="mt-1">
-                <div class="d-flex gap-2 justify-content-start">
-                    <div>
-                        <a href="<?= site_url('Siswa/Materi/Page') ?>" class="text-decoration-none text-reset">
-                            <div class="box-icon bg-pink">
-                                <img src="<?= base_url('assets/image/siswa/materi_teks.png') ?>" class="img-fluid" style="height: 50px; width:50px;">
-                                <p class="m-0 font-custom">Materi Teks</p>
+                <div class="carousel-item">
+                    <div class="mt-2">
+                        <div class="row">
+                            <div class="slide col-sm-6 col-md-4">
+                                <p class="mb-0">desktop 1</p>
+                                <div class="position-relative box-content bg-yellow box-custom">
+                                    <div class="mt-3">
+                                        <p class="m-0">Selamat Datang di Digital Learning <br> Mata Pelajaran
+                                            Teknologi, Informasi,<br> dan Komunikasi (TIK)</p>
+                                        <img src="<?= base_url('assets/image/siswa/world.png') ?>" class="position-absolute bottom-left-image" style="margin-top: 2rem;" width="70">
+                                        <img src="<?= base_url('assets/image/siswa/stack_of_books.png') ?>" class="position-absolute bottom-right-image" style="margin-bottom: 2rem;" width="60">
+                                    </div>
+                                </div>
                             </div>
-                        </a>
-                    </div>
 
-                    <div>
-                        <a href="<?= site_url('Siswa/MateriVideo/Page') ?>" class="text-decoration-none text-reset">
-                            <div class="box-icon bg-yellow">
-                                <img src="<?= base_url('assets/image/siswa/materi_video.png') ?>" class="img-fluid" style="height: 50px; width:50px;">
-                                <p class="m-0 font-custom">Materi Video</p>
+                            <div class="col-sm-6 col-md-4">
+                                <p class="mb-0">desktop 2</p>
+                                <div class="position-relative box-content bg-blue box-custom">
+                                    <div class="mt-3">
+                                        <p class="m-0">Waktunya Untuk Belajar <br> “Dengan ilmu, kamu bisa meraih <br> apa pun yang kamu impikan”</p>
+                                        <img src="<?= base_url('assets/image/siswa/brain.png') ?>" class="position-absolute bottom-left-image" style="margin-top: 2rem;" width="110">
+                                        <img src="<?= base_url('assets/image/siswa/thinking.png') ?>" class="position-absolute bottom-right-image" style="margin-bottom: 3rem;" width="100">
+                                    </div>
+                                </div>
                             </div>
-                        </a>
-                    </div>
 
-                    <div>
-                        <a href="<?= site_url('Siswa/Praktikum/Page') ?>" class="text-decoration-none text-reset">
-                            <div class="box-icon bg-blue">
-                                <img src="<?= base_url('assets/image/siswa/praktikum.png') ?>" class="img-fluid" style="height: 50px; width:50px;">
-                                <p class="m-0 font-custom">Praktikum</p>
+                            <div class="col-sm-6 col-md-4">
+                                <p class="mb-0">desktop 3</p>
+                                <div class="position-relative box-content bg-pink box-custom">
+                                    <div class="mt-1">
+                                        <p class="m-0">"Pergi berbelanja ke pasar<br>
+                                            Saking besarnya jadi kesasar <br>
+                                            Jika ingin menjadi pintar <br>
+                                            Tentulah harus rajin belajar
+                                        </p>
+                                        <img src=" <?= base_url('assets/image/siswa/student.png') ?>" class="position-absolute bottom-left-image" style="margin-top: 3rem;" width="80">
+                                        <img src=" <?= base_url('assets/image/siswa/star.png') ?>" class="position-absolute bottom-right-image" style="margin-bottom: 2rem;" width="100">
+                                    </div>
+                                </div>
                             </div>
-                        </a>
-                    </div>
-
-                    <div>
-                        <a href="<?= site_url('Siswa/Nilai/Page') ?>" class="text-decoration-none text-reset">
-                            <div class="box-icon bg-green">
-                                <img src="<?= base_url('assets/image/siswa/nilai.png') ?>" class="img-fluid" style="height: 50px; width:50px;">
-                                <p class="m-0 font-custom">Nilai</p>
-                            </div>
-                        </a>
+                        </div>
                     </div>
                 </div>
+
             </div>
-            <div class="col-md-2">
-                <a href="<?= site_url('login/logout') ?>" class="btn btn-secondary mt-4 btn-custom" style="border-radius: 10px; height:7vh">Logout</a>
+            <!-- carousel End-->
+
+            <div class="mt-2 overflow-hidden">
+
+                <div class="box-content bg-yellow box-custom" style="height: auto;">
+                    <div class="mt-3">
+
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div class="row">
+                                <div class="col-sm-6 col-md-2 m-5">
+                                    <a href="<?= site_url('Siswa/Materi/Page') ?>" class="text-decoration-none text-reset">
+                                        <div class="box-icon bg-pink" style="height: 250px;width:250px;border-radius:10px;">
+                                            <img src="<?= base_url('assets/image/siswa/materi_teks.png') ?>" class="img-fluid" width="70%">
+                                            <p class="m-0 font-custom mt-2">Materi Teks</p>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-sm-6 col-md-2 m-5">
+                                    <a href="<?= site_url('Siswa/Praktikum/Page') ?>" class="text-decoration-none text-reset">
+                                        <div class="box-icon bg-blue" style="height: 250px;width:250px;border-radius:10px;">
+                                            <img src="<?= base_url('assets/image/siswa/praktikum.png') ?>" class="img-fluid" width="75%">
+                                            <p class="m-0 font-custom">Praktikum</p>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-sm-6 col-md-2 m-5">
+                                    <a href="<?= site_url('Siswa/MateriVideo/Page') ?>" class="text-decoration-none text-reset">
+                                        <div class="box-icon bg-dark-yellow" style="height: 250px;width:250px;border-radius:10px;margin-left:1rem;">
+                                            <img src="<?= base_url('assets/image/siswa/materi_video.png') ?>" class="img-fluid" width="70%">
+                                            <p class="m-0 font-custom mt-2">Materi Video</p>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-sm-6 col-md-2 m-5">
+                                    <a href="<?= site_url('Siswa/Nilai/Page') ?>" class="text-decoration-none text-reset">
+                                        <div class="box-icon bg-green" style="height: 250px;width:250px;border-radius:10px;margin-left:1rem;">
+                                            <img src="<?= base_url('assets/image/siswa/nilai.png') ?>" style="margin-top:0.5rem;" class="img-fluid" width="65%">
+                                            <p class="m-0 font-custom mt-4">Nilai</p>
+                                        </div>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="d-flex justify-content-end">
+                            <a href="<?= site_url('login/logout') ?>" class="btn mt-4 btn-custom" style="background:white; border-radius: 10px; height:5vh">Keluar</a>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
         </div>
     </div>
 
