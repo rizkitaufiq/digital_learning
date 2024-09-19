d79e9200f0b6648698bc2d76c91aed73bee7e3d6
rizkitaufiqgit config --global user.email <rizkitaufiq97@gmail.com>
2024-09-20 00:55:39 +0700

    update content

--- application/views/admin/siswa/update.php
+++ application/views/admin/siswa/update.php
@@ -25,42 +25,35 @@
     <div class="container mt-5">
         <div class="card">
             <div class="card-body bg-card">
-                <div class="m-1">
-                    <h4>Tambah Siswa</h4>
-                </div>
 
                 <form action="<?= site_url('Admin/Siswa/Update/Process') ?>" method="post">
                     <?php foreach ($data as $item) { ?>
-                        <input type="hidden" name="id_siswa" value="<?php echo $item->id_siswa ?>" placeholder="Nama Siswa :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                        <input type="hidden" name="id_siswa" value="<?php echo $item->id_siswa ?>" placeholder="Nama Siswa :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         <div class="form-group">
-                            <input type="text" name="nama" value="<?php echo $item->nama ?>" placeholder="Nama Siswa :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">Nama Siswa</label>
+                            <input type="text" name="nama" value="<?php echo $item->nama ?>" placeholder="Nama Siswa :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
 
                         <div class="form-group">
-                            <input type="number" name="nisn" value="<?php echo $item->nisn ?>" placeholder="NISN :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">NISN</label>
+                            <input type="number" name="nisn" value="<?php echo $item->nisn ?>" placeholder="NISN :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="kelas" value="<?php echo $item->kelas ?>" placeholder="Kelas :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">Kelas</label>
+                            <input type="text" name="kelas" value="<?php echo $item->kelas ?>" placeholder="Kelas :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="tempat_lahir" value="<?php echo $item->tempat_lahir ?>" placeholder="Tempat Lahir :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="date" name="tanggal_lahir" value="<?php echo $item->tanggal_lahir ?>" placeholder="Tanggal Lahir :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
-                        </div>
-
-                        <div class="form-group">
+                            <label for="nama">Jenis Kelamin</label>
                             <?php if ($item->jenis_kelamin == "L") { ?>
-                                <select name="jenis_kelamin" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                                <select name="jenis_kelamin" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                                     <option value="L" <?php echo set_select('jenis_kelamin', 'L'); ?>>L</option>
                                     <option value="P" <?php echo set_select('jenis_kelamin', 'P'); ?>>P</option>
                                 </select>
 
                             <?php } else { ?>
-                                <select name="jenis_kelamin" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                                <select name="jenis_kelamin" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                                     <option value="P" <?php echo set_select('jenis_kelamin', 'P'); ?>>P</option>
                                     <option value="L" <?php echo set_select('jenis_kelamin', 'L'); ?>>L</option>
                                 </select>
@@ -68,23 +61,35 @@
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="agama" value="<?php echo $item->agama ?>" placeholder="Agama :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">Tempat Lahir</label>
+                            <input type="text" name="tempat_lahir" value="<?php echo $item->tempat_lahir ?>" placeholder="Tempat Lahir :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="alamat" value="<?php echo $item->alamat ?>" placeholder="Alamat :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">Tanggal, Bulan, Tahun Lahir</label>
+                            <input type="date" name="tanggal_lahir" value="<?php echo $item->tanggal_lahir ?>" placeholder="Tanggal Lahir :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="username" value="<?php echo $item->username ?>" placeholder="Username :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">Agama</label>
+                            <input type="text" name="agama" value="<?php echo $item->agama ?>" placeholder="Agama :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="password" value="<?php echo $item->password ?>" placeholder="Password :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 0; border: 1px solid #000000">
+                            <label for="nama">Alamat</label>
+                            <input type="text" name="alamat" value="<?php echo $item->alamat ?>" placeholder="Alamat :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
                         </div>
+
+                        <!-- <div class="form-group">
+                            <input type="text" name="username" value="<?php echo $item->username ?>" placeholder="Username :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" name="password" value="<?php echo $item->password ?>" placeholder="Password :" class="form-control mb-2" style="background: #F9FAC7 ; border-radius: 10px; border: 1px solid #000000">
+                        </div> -->
 
                         <div class="form-group d-flex justify-content-end align-item-center">
-                            <button type="submit" class="btn btn-info" style="border-radius: 20px;border: 1px solid #000000; width:16vh; height:5vh">Simpan</button>
+                            <button type="submit" class="btn " style="border-radius: 20px;width:16vh; height:5vh">Simpan</button>
                         </div>
                     <?php } ?>
                 </form>
